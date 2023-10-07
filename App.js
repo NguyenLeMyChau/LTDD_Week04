5e1f7600f1cbb89506b5935ca57cbd5ed75bee31
My Chau <nguyenlemychau1201@gmail.com>
2023-10-08 00:55:15 +0700

    Full week04

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Pressable } from 'react-native';
+import LoginScreen from './src/components/LoginScreen';
+import PasswordGenerateScreen from './src/components/PasswordGenerateScreen';
+import VoteScreen from './src/components/VoteScreen';
+import PayScreen from './src/components/PayScreen';
 
-export default function App() {
+export default function App({}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //<LoginScreen/>
+    //<VoteScreen/>
+    //<PasswordGenerateScreen/>
+    <PayScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
